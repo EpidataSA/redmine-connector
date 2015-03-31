0b96e3d8f822feec6ce5842d94b2d437728b03c9
Fernando Oliveto <fernandoo@epidataconsulting.com>
2015-03-30 22:06:23 -0300

    First version of files

---
+++ target/generated-sources/mule/org/mule/modules/redmine/devkit/SplashScreenAgent.java
@@ -0,0 +1,26 @@
+
+package org.mule.modules.redmine.devkit;
+
+import javax.annotation.Generated;
+
+
+/**
+ * Marks DevKit {@link org.mule.api.agent.Agent} implementations to take care of logging information at Mule app level mainly for troubleshooting purposes.
+ * 
+ */
+@SuppressWarnings("all")
+@Generated(value = "Mule DevKit Version 3.6.1", date = "2015-03-30T04:57:42-03:00", comments = "Build UNNAMED.2405.44720b7")
+public interface SplashScreenAgent {
+
+      /**
+     * Print version information for all the modules used by the application
+     */
+    void splash();
+
+    /**
+     * Retrieve the count of modules used by the application
+     *
+     * @return
+     */
+    int getExtensionsCount();
+}
